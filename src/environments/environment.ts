bf56e4af02f187ed9c27c2ab2b9f2d6c11f08c9c
Syd <blubberbo@gmail.com>
2022-04-28 17:15:35 -0700

    remove the environment files

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-   production: false,
-   faundadbGraphQLEndpoint: 'https://graphql.fauna.com/graphql',
-   faundadbSecret: "fnAElJ-15_ACVBU5HV_98tcq8QPbqMGfeFFmDhtw",
-};
